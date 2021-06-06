4dbe2ce6975ac5dc14761648ef968de6112f6bd1
Dalarin <artem.shipunov1995@gmail.com>
2021-06-06 17:48:28 +0300

    Сохранил код

--- src/SwingDemo.java
+++ src/SwingDemo.java
@@ -1,45 +1,63 @@
-import java.awt.Color;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.sql.Savepoint;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.UIManager;
+import javax.swing.*;
 
 public class SwingDemo {
     static JFrame frame = new JFrame("Encryption");
+    static JPanel panel = new JPanel();
     static JMenuBar menuBar = new JMenuBar();
     static JMenuBar mb = new JMenuBar();
     JMenu fileMenu = new JMenu("Меню");
     JMenu fileMenuforWork = new JMenu("Файл");
     JMenuItem openFile = new JMenuItem("Открыть", KeyEvent.VK_N);
     JMenuItem saveFile = new JMenuItem("Сохранить", KeyEvent.VK_N);
+    JMenu settingsMenu = new JMenu("Настройки");
+    JMenuItem openMenu = new JMenuItem("Настройки");
     JMenu reference = new JMenu("Справка");
     JMenuItem aboutProgram = new JMenuItem("О программе");
     JMenuItem aboutDeveloper = new JMenuItem("О разработчике");
+    JButton encrypt = new JButton("Зашифровать");
+    JButton decrypt = new JButton("Дешифровать");
 
-    SwingDemo()
-    {
-        MenuEngine menuEngine = new MenuEngine(this);
+    public void CreateButtons() {
+        panel.add("South", encrypt);
+        panel.add("South", decrypt);
+        panel.revalidate();
+
+    }
+
+    SwingDemo() {
+        RSAEncryption child = new RSAEncryption();
+        MenuEngine menuEngine = new MenuEngine(this, child);
+        GridLayout gl = new GridLayout(12, 12);
+        panel.setLayout(gl);
         fileMenu.setMnemonic(KeyEvent.VK_F);
         menuBar.add(fileMenu);
         fileMenu.add(fileMenuforWork);
         fileMenuforWork.add(openFile);
         fileMenuforWork.add(saveFile);
         fileMenuforWork.setMnemonic(KeyEvent.VK_F);
+        menuBar.add(settingsMenu);
         menuBar.add(reference);
         reference.add(aboutDeveloper);
         reference.add(aboutProgram);
+        settingsMenu.add(openMenu);
         openFile.addActionListener(menuEngine);
+        openMenu.addActionListener(menuEngine);
     }
+
     public static void main(final String[] args) {
         SwingDemo demo = new SwingDemo();
+        panel.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         UIManager.put("MenuBar.background", Color.ORANGE);// цвет для верхней панели
         frame.setJMenuBar(mb);
         frame.setJMenuBar(menuBar);
         frame.setSize(450, 450);
         frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
--- src/menuEngine.java
+++ src/menuEngine.java
@@ -1,25 +1,81 @@
-import java.awt.Component;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JMenuItem;
-import javax.swing.JFileChooser;
-import java.io.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.*;
+import java.util.List;
+import java.util.Scanner;
+
+class RSAEncryption {
+    public PrivateKey genPrivateKey() throws NoSuchAlgorithmException {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        KeyPair pair = generator.generateKeyPair();
+        return pair.getPrivate();
+    }
+
+
+    public void savePrivateKey() throws NoSuchAlgorithmException, IOException {
+        try (FileOutputStream fos = new FileOutputStream("private.der")) {
+            fos.write(genPrivateKey().getEncoded());
+        }
+    }
+
+    private static String File2String(File fileName) throws java.io.IOException {
+        File file = new File(String.valueOf(fileName));
+        char[] buffer = null;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        buffer = new char[(int) file.length()];
+        int i = 0;
+        int c = bufferedReader.read();
+        while (c != -1) {
+            buffer[i++] = (char) c;
+            c = bufferedReader.read();
+        }
+        return new String(buffer);
+    }
+
+
+    private PrivateKey readRSAPEMKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] keyBytes = Files.readAllBytes(Paths.get("private.der"));
+        PKCS8EncodedKeySpec spec =
+                new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        System.out.println(kf.generatePrivate(spec));
+        return kf.generatePrivate(spec);
+    }
+
+
+    public void generatePublicKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        PrivateKey myPrivateKey = readRSAPEMKey();
+        RSAPrivateCrtKeySpec privk = (RSAPrivateCrtKeySpec) myPrivateKey;
+        RSAPublicKeySpec publicKeySpec = new java.security.spec.RSAPublicKeySpec(privk.getModulus(), privk.getPublicExponent());
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey myPublicKey = keyFactory.generatePublic(publicKeySpec);
+        System.out.println(myPublicKey);
+
+    }
+}
 
 class MenuEngine extends Component implements ActionListener {
     SwingDemo parent;
+    RSAEncryption child;
 
-    MenuEngine(SwingDemo parent) {
+    MenuEngine(SwingDemo parent, RSAEncryption child) {
         this.parent = parent;
+        this.child = child;
     }
 
-    public void parseDOCXFile(File file) throws IOException {
+    private static boolean statusOfFile = false;
+
+    private void parseDOCXFile(File file) {
         String textofFile = "";
         try {
             File newfile = new File(String.valueOf(file));
@@ -35,7 +91,15 @@ class MenuEngine extends Component implements ActionListener {
         }
     }
 
-    public void parseTXTFile(File file) throws IOException {
+    private void createSettingsWindow() {
+        JFrame frames = new JFrame("Настройки");
+        frames.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frames.setSize(300, 300);
+        frames.setVisible(true);
+
+    }
+
+    private void parseTXTFile(File file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         StringBuilder stringBuilder = new StringBuilder();
         String line = null;
@@ -55,7 +119,6 @@ class MenuEngine extends Component implements ActionListener {
     private void getPathFile() throws IOException {
         JFileChooser fileChooser = new JFileChooser();
         File selectedFile = null;
-        boolean statusOfFile = false;
         Scanner in = null;
         if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
             selectedFile = fileChooser.getSelectedFile();
@@ -70,8 +133,10 @@ class MenuEngine extends Component implements ActionListener {
         if (statusOfFile) { // тут надо разбить на строку и проверять txt или docx
             if (parts[1].equals("txt")) {
                 parseTXTFile(selectedFile);
+                parent.CreateButtons();
             } else if (parts[1].equals("docx")) {
                 parseDOCXFile(selectedFile);
+                parent.CreateButtons();
             }
         }
     }
@@ -86,6 +151,13 @@ class MenuEngine extends Component implements ActionListener {
                 getPathFile();
             } catch (IOException ioException) {
                 ioException.printStackTrace();
+            }
+        } else if (src == parent.openMenu) {
+            createSettingsWindow();
+            try {
+                child.generatePublicKey();
+            } catch (NoSuchAlgorithmException | IOException | InvalidKeySpecException noSuchAlgorithmException) {
+                noSuchAlgorithmException.printStackTrace();
             }
         }
     }
